dba17e359bb1508695e4ad3404275b3715574a18
Rahul Nayak <132944744+Rahul-web087@users.noreply.github.com>
2025-12-23 20:14:06 +0530

    Change API key to placeholder in weather_api.py
    Updated API key placeholder for security.

--- weather_api.py
+++ weather_api.py
@@ -1,6 +1,6 @@
 import requests
 city_name = 'Jajpur'
-api_key = '2146f3d92970f96826eef505c22d4b36'
+api_key = 'you_api_key'
 url =f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric'
 response = requests.get(url)
 if response.status_code == 200:
@@ -11,4 +11,5 @@ if response.status_code == 200:
     print("Wind Speed is ",data["wind"]["speed"])
     print("Wind Direction is ",data["wind"]["deg"])
     print("Weather Condition is ",data["weather"][0]["main"])
+
     print("Weather Condition is ",data["weather"][0]["description"])
